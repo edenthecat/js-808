1eac4bec4f8691b594dea7a2cb9b89cd7f4b9ea0
Eden Rohatensky <edenrohatensky@gmail.com>
2017-09-30 15:05:24 -0400

    Started fleshing out ideas for js.

---
+++ machine.js
@@ -0,0 +1,60 @@
+$(document).ready(function(){
+  /*
+  * TESTING DATA
+  */
+
+  /*
+  * UI INTERACTION & EVENT LISTENERS
+  */
+
+
+  /*
+  * When a user clicks on a cell, it should activate that cell.
+  */
+
+  /*
+  * When a user clicks on the play button:
+  * - If not playing, it should play.
+  * - If playing, it should pause.
+  */
+
+  /*
+  * Event: Row Playing
+  */
+
+  /*
+  * Event: Row Paused
+  */
+
+  /*
+  * FUNCTIONALITY
+  */
+
+  /*
+  * playRow: plays a row
+  */
+
+  /*
+  * getPlayFrequency: calculates the play frequency based on the BPM
+  */
+
+  /*
+  * Set current cell
+  */
+
+  /*
+  * Get current cell
+  */
+
+  /*
+  * getCellByIndices: given a set of indices, returns the specified cell (jQuery object)
+  */
+
+  /*
+  * getNextCell: retrieves the next cell in a row
+  */
+
+  /*
+  * playCell: checks if a cell is active, and if it is, sets the state to hit.
+  */
+});
