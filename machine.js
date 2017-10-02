4c41ab7436bcf5ac26ceb27f7b20f35e2543e79b
Eden Rohatensky <edenrohatensky@gmail.com>
2017-10-01 20:03:00 -0400

    Added ability to load presets and dynamically generate the grid. Needs cleanup - also for some reason step 0 doesn't play the first time around.

--- machine.js
+++ machine.js
@@ -5,6 +5,112 @@ $(document).ready(function(){
   var bpm = 60;
   var barLength = 8;
   var step = 0;
+  var totalSteps = 0;
+  var presetFile = "presets.json";
+  var instrumentFile = "instruments.json";
+  var instrumentList = [
+    "kick",
+    "snare",
+    "rim click",
+    "tom 1",
+    "tom 2",
+    "floor tom",
+    "hi-hat",
+    "open hi-hat",
+    "ride",
+    "ride bell",
+    "hi-hat foot"];
+
+  /*
+  * INITIALIZATION
+  */
+  $(".grid").ready(function() {
+    presetList = getPresetList(presetFile);
+    loadPreset("rock beat");
+  });
+
+  function generateInstrumentSelect() {
+    var $instrumentSelect = $("<select/>", {"class": "instrument-select"});
+    for (var i=0; i < instrumentList.length; i++) {
+      $option = $("<option/>")
+        .val(instrumentList[i])
+        .html(instrumentList[i])
+        .appendTo($instrumentSelect);
+    }
+    return $instrumentSelect;
+  }
+
+  function getPresetList() {
+    $.getJSON(presetFile, function( data ) {
+      var presets = data.presets;
+      var presetNames = Object.keys(presets);
+      for (var i=0; i < presetNames.length; i++){
+        $('<option/>')
+          .val(presetNames[i])
+          .html(presetNames[i])
+          .appendTo('#presets');
+      }
+    });
+  }
+
+  function loadPreset(presetName) {
+    $.getJSON(presetFile, function( data ) {
+      var presets = Object.entries(data.presets);
+      var preset = presets.find( function(item) {
+        return item[0] == presetName;
+      });
+      displayPreset(preset[1]);
+    });
+  }
+
+  function displayPreset(preset) {
+    var instruments = preset.instruments;
+
+    clearMachine();
+    generateGridFromPreset(instruments);
+  }
+
+  function clearMachine() {
+    $(".grid").empty();
+  }
+
+  function generateGridFromPreset(instruments) {
+    for (var instrumentName in instruments) {
+      var $row = generateRow(instrumentName);
+
+      var bars = instruments[instrumentName];
+      for (var i=0; i < bars.length; i++) {
+        $bar = generateBar(i);
+        for(var j=0; j < bars[i].length; j++) {
+          if(bars[i][j] == 1) {
+            var $cell = $($bar.children()[j]);
+            $cell.addClass("active");
+          }
+        }
+        $bar.appendTo($row);
+      }
+      $row.appendTo($(".grid"));
+    }
+  }
+
+  function generateRow(instrumentName) {
+    var rowIndex = 0;
+    if ($(".row").length > 0) {
+      rowIndex = $(".row").last().data("row") + 1;
+    }
+    $row = $("<div>", {"class": "row", "data-row": rowIndex});
+    $instrumentSelect = generateInstrumentSelect();
+    //if (instrumentName) {
+    //  $instrumentSelect.val(instrumentName);
+    //}
+    //$instrumentSelect.appendTo($row);
+
+    $row.on("row:play", function() {
+       $this = $(this);
+       playRow($this);
+    });
+    return $row;
+  }
 
   /*
   * UI INTERACTION & EVENT LISTENERS
@@ -13,9 +119,7 @@ $(document).ready(function(){
   /*
   * When a user clicks on a cell, that cell is set to active.
   */
-  $(".cell").click(function() {
-    $(this).toggleClass("active");
-  });
+
 
   /*
   * When a user clicks on the play button:
@@ -24,13 +128,12 @@ $(document).ready(function(){
   */
   $(".play-button").click(function() {
     var $this = $(this);
-    var grid = $(".grid");
 
     $this.toggleClass("playing");
     if($this.hasClass("playing")) {
-      grid.trigger("machine:playing");
+      $(".grid").trigger("machine:playing");
     } else {
-      grid.trigger("machine:paused");
+      $(".grid").trigger("machine:paused");
     }
   });
 
@@ -58,14 +161,6 @@ $(document).ready(function(){
     clearInterval(playId);
   });
 
-  $(".row").on("row:play", function() {
-    console.log("row playing");
-    $this = $(this);
-    console.log($this);
-
-    playRow($this);
-  });
-
   /*
   * FUNCTIONALITY
   */
@@ -75,7 +170,6 @@ $(document).ready(function(){
   */
   function play() {
     rows = $(".grid").children(".row");
-    console.log("is playing");
     rows.trigger("row:play");
     incrementStep();
   }
@@ -83,16 +177,23 @@ $(document).ready(function(){
   * playRow: plays a row
   */
   function playRow(row) {
-    var currentCell = getCurrentCell(row);
-    console.log(row.data("row"));
+    console.log("playing row: " + step);
+    if(step == 0 && totalSteps == 0) {
+      var currentCell = getCellByIndices(row.data("row"), 0, step);
+      currentCell.addClass("current");
+    } else {
+      var currentCell = getCurrentCell(row);
+    }
     var currentBar = $(currentCell).parent();
+
 
     var nextCell = getNextCell(row, currentBar, currentCell);
     var nextBar = nextCell.parent();
+
     setCurrentBar(row, nextBar);
 
     setCurrentCell(row, nextCell);
-    return true;
+    //return true;
   }
 
   /*
@@ -109,18 +210,21 @@ $(document).ready(function(){
   * Set current step
   */
   function incrementStep() {
+    console.log("incrementing step");
     if(step < 7) {
       step++;
     } else {
       step = 0;
     }
+    totalSteps++;
   }
 
   /*
   * Set current cell
   */
   function setCurrentCell(row, cell) {
-    row.children().children().removeClass("current");
+    $bars = getCurrentBar(row);
+    $bars.children().removeClass("current");
     cell.addClass("current");
   }
 
@@ -138,13 +242,20 @@ $(document).ready(function(){
   }
 
   function setCurrentBar(row, bar) {
-    console.log(bar);
-    bar.siblings().removeClass("current");
-    bar.addClass("current");
+    if(!bar.hasClass("current")) {
+      $bars = $(row.children());
+      $bars.removeClass("current");
+      bar.addClass("current");
+    }
   }
 
   function getCurrentBar(row) {
-    return $(row.children(".current"));
+    $bar = $(row.children(".current"));
+    if($bar.length == 0) {
+      $bar = $(row.children().get(0));
+      setCurrentBar(row, $bar);
+    }
+    return $bar;
   }
 
   /*
@@ -191,9 +302,6 @@ $(document).ready(function(){
     $bar.appendTo($row);
 
     $row.appendTo($(".grid"));
-
-
-    console.log($row);
   }
 
   function generateBar(barIndex) {
@@ -201,9 +309,15 @@ $(document).ready(function(){
 
     for (i = 0; i < barLength; i++) {
       var $cell = $("<div>", {"class": "cell", "data-cell": i});
+
+      $cell.click(function() {
+          $(this).toggleClass("active");
+      });
+
       $cell.appendTo($bar);
     }
 
     return $bar;
   }
+
 });
